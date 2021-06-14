e5afa660646053e5fa329ef3711c98ec6a35a919
pit <asgrwa@easafda.com>
2021-06-14 12:20:35 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {ApolloClient, InMemoryCache, gql, useQuery} from '@apollo/client';
+
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'https://countries.trevorblades.com'
+});
+const LIST_COUNTRIES = gql`
+  {
+    countries {
+      name
+      native
+      emoji
+      currency
+      languages{
+          code
+          name
+        }
+    }
+  }
+`;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   const {data, loading, error} = useQuery(LIST_COUNTRIES, {client});
+
+console.log(data);
+function getDeatils(country) {
+  alert(`This country name is: ${country.name}.
+        The currency is: ${country.currency}.
+        The native is: ${country.native}.
+        the languaje is: ${country.languages[0].name}
+  `
+  
   );
 }
+if (loading || error) {
+  return <p>{error ? error.message : 'Loading...'}</p>;
+}
+
+
+
+  return ( 
+    <div className="App">
+        { data.countries.map(country =>{
+         return ([
+         <div>
+           <p key={country.code}>{country.name}</p> 
+          <button  onClick={() => getDeatils(country)}  >details</button>
+           </div>
+         
+         ])
+        })
+        }
+    </div>
+  )
+
+  
+
+
+}
+
+
 
 export default App;
